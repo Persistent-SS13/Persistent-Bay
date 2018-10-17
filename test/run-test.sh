e6c25184e139d16ad99af781065e47ba1ca19dc6
Filipe Reis <filipereis233@gmail.com>
2018-10-17 20:53:59 +0100

    Changes the hash on global_access.dm

--- test/run-test.sh
+++ test/run-test.sh
@@ -242,7 +242,7 @@ function run_byond_tests {
         source $HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin/byondsetup
     fi
     run_test_ci "check globals build" "python3 tools/GenerateGlobalVarAccess/gen_globals.py persistentss13.dme code/_helpers/global_access.dm"
-    run_test "check globals unchanged" "md5sum -c - <<< '3645afc96bec5647e43cdc9db73be552 *code/_helpers/global_access.dm'"
+    run_test "check globals unchanged" "md5sum -c - <<< '2a3bb087bb405e816b3155fe83b111c9 *code/_helpers/global_access.dm'"
     run_test "build map unit tests" "scripts/dm.sh -DUNIT_TEST -M$MAP_PATH persistentss13.dme"
     run_test "check no warnings in build" "grep ', 0 warnings' build_log.txt"
     run_test "run unit tests" "DreamDaemon persistentss13.dmb -invisible -trusted -core 2>&1 | tee log.txt"
